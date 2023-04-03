2e24ea8786de91090a8f2cde62be5cd8b013a3fa
Jossy1238 <106344230+Jossy1238@users.noreply.github.com>
2023-04-03 22:43:40 +0400

    Update

--- JAVASCRIPT/countries.js
+++ JAVASCRIPT/countries.js
@@ -66,22 +66,21 @@ searchBtn.addEventListener("click", () => {
                         <span>${data[0].capital[0]}</span>
                     </div>
                 </div>
-                
-                <div class="wrapper">
-                    <div class="data-wrapper">
-                        <h4>Capital:</h4>
-                        <div id="map"> </div>
-                    </div>
-                </div>
                 <div class="wrapper">
                     <div class="data-wrapper">
                         <h4>Continent:</h4>
                         <span>${data[0].continents[0]}</span>
                     </div>
                 </div>
-            </div>
-
-             <div class="right">
+                <div class="wrapper">
+                    <div class="data-wrapper">
+                        <h4>Common Languages:</h4>
+                        <span>${Object.values(data[0].languages)
+                        .toString()
+                        .split(",")
+                        .join(", ")}</span>
+                    </div>
+                </div>
                 <div class="wrapper">
                     <div class="data-wrapper">
                         <h4>Population:</h4>
@@ -96,15 +95,74 @@ searchBtn.addEventListener("click", () => {
                         } - ${Object.keys(data[0].currencies)[0]}</span>
                     </div>
                 </div>
-                <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4>Coat Of Arms:</h4>
+                    <img src="${data[0].coatOfArms.svg}" class="flag-img">
+                </div>
+                
+                
+            </div>
+
+          <div class="right">
+             <div class="wrapper">
+             <div class="data-wrapper">
+               <h4>Total land Area:</h4>
+               <span>${data[0].area}</span>
+             </div>
+           </div>
+
+           <div class="wrapper">
+               <div class="data-wrapper">
+               <h4>Start of Week:</h4>
+               <span>${data[0].startOfWeek}</span>
+               </div>
+           </div>
+
+           <div class="wrapper">
+               <div class="data-wrapper">
+               <h4>Latitude:</h4>
+               <span>${data[0].latlng[0]}</span>
+               </div>
+           </div>
+
+           <div class="wrapper">
+               <div class="data-wrapper">
+               <h4>Longitude:</h4>
+               <span>${data[0].latlng[1]}</span>
+               </div>
+           </div>
+
+           <div class="wrapper">
+               <div class="data-wrapper">
+               <h4>Timezones:</h4>
+               <span>${data[0].timezones[0]}</span>
+               </div>
+           </div>
+
+           <div class="wrapper">
+               <div class="data-wrapper">
+               <h4>Subregion:</h4>
+               <span>${data[0].subregion}</span>
+               </div>
+           </div>
+
+
+           <div class="wrapper">
                     <div class="data-wrapper">
-                        <h4>Common Languages:</h4>
-                        <span>${Object.values(data[0].languages)
+                        <h4>Borders with other countries:</h4>
+                        <span>${Object.values(data[0].borders)
                         .toString()
                         .split(",")
                         .join(", ")}</span>
                     </div>
-                </div>
+            </div>
+
+            <div class="wrapper">
+                    <div class="data-wrapper">
+                        <div id="map"> </div>
+                    </div>
+            </div>
+                
             </div>
         </div>
 
@@ -112,12 +170,122 @@ searchBtn.addEventListener("click", () => {
       `;
 
       cuntriesMap(data[0].latlng[0], data[0].latlng[1]);
+    })
+    .catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+      } else {
+        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+      }
+
+    // hide the error after 3 seconds
+    setTimeout(() => {
+      result.classList.add('hidden');
+     }, 3000);
+    });
+});
 
 
+  /*Js for chart*/
+// Define the API endpoint to fetch the data
+ const url = 'https://restcountries.com/v3.1/all';
+
+ // Fetch the data using Fetch API
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const continents = {};
+    data.forEach(country => {
+      const continent = country.region;
+      if (continent in continents) {
+        continents[continent] += 1;
+      } else {
+        continents[continent] = 1;
+      }
+    });
+    const chartData = {
+      labels: Object.keys(continents),
+      datasets: [{
+        label: 'Total Number of Countries per Continent',
+        data: Object.values(continents),
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1
+      }]
+    };
+    const chartOptions = {
+      scales: {
+        y: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      legend: {
+        display: false
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      barThickness: 30 // Adjust the bar thickness as needed
+    };
+    const chart = new Chart(document.getElementById('chart'), {
+      type: 'bar',
+      data: chartData,
+      options: chartOptions
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
 
 
+/*Js for chart*/
+// Define the API endpoint to fetch the data
+// const url = 'https://restcountries.com/v3.1/all';
 
-    // result.innerHTML =`
+// Fetch the data using Fetch API
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
+    // Process the data to get the total number of countries in each continent
+    // const continentMap = new Map();
+    // data.forEach(country => {
+    //   const continent = country.region;
+    //   if (continent in continentMap) {
+    //     continentMap[continent]++;
+    //   } else {
+    //     continentMap[continent] = 1;
+    //   }
+    // });
+
+    // Create a new chart using Chart.js
+  //   const ctx = document.getElementById('chart').getContext('2d');
+  //   const chart = new Chart(ctx, {
+  //     type: 'bar',
+  //     data: {
+  //       labels: Object.keys(continentMap),
+  //       datasets: [{
+  //         label: 'Total number of countries per continent',
+  //         data: Object.values(continentMap),
+  //         backgroundColor: 'rgba(54, 162, 235, 0.2)',
+  //         borderColor: 'rgba(54, 162, 235, 1)',
+  //         borderWidth: 1
+  //       }]
+  //     },
+  //     options: {
+  //       scales: {
+  //         y: {
+  //           ticks: {
+  //             beginAtZero: true
+  //           }
+  //         }
+  //       }
+  //     }
+  //   });
+  // });
+
+
+   // result.innerHTML =`
 
     //         <h2 class="text-heading text-3xl font-bold">${data[0].name.common}</h2>
     //         <div class="main">
@@ -233,115 +401,3 @@ searchBtn.addEventListener("click", () => {
     //     loadMap(data[0].latlng[0], data[0].latlng[1], data[0].name.common);
 
     //     countryInp.value = "";
-    })
-    .catch(() => {
-      if (countryName.length == 0) {
-        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
-      } else {
-        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
-      }
-    });
-});
-
-
-
-
-
-/*Js for chart*/
-// Define the API endpoint to fetch the data
-// const url = 'https://restcountries.com/v3.1/all';
-
-// Fetch the data using Fetch API
-// fetch(url)
-//   .then(response => response.json())
-//   .then(data => {
-    // Process the data to get the total number of countries in each continent
-    // const continentMap = new Map();
-    // data.forEach(country => {
-    //   const continent = country.region;
-    //   if (continent in continentMap) {
-    //     continentMap[continent]++;
-    //   } else {
-    //     continentMap[continent] = 1;
-    //   }
-    // });
-
-    // Create a new chart using Chart.js
-  //   const ctx = document.getElementById('chart').getContext('2d');
-  //   const chart = new Chart(ctx, {
-  //     type: 'bar',
-  //     data: {
-  //       labels: Object.keys(continentMap),
-  //       datasets: [{
-  //         label: 'Total number of countries per continent',
-  //         data: Object.values(continentMap),
-  //         backgroundColor: 'rgba(54, 162, 235, 0.2)',
-  //         borderColor: 'rgba(54, 162, 235, 1)',
-  //         borderWidth: 1
-  //       }]
-  //     },
-  //     options: {
-  //       scales: {
-  //         y: {
-  //           ticks: {
-  //             beginAtZero: true
-  //           }
-  //         }
-  //       }
-  //     }
-  //   });
-  // });
-
-
-
-  /*Js for chart*/
-// Define the API endpoint to fetch the data
- const url = 'https://restcountries.com/v3.1/all';
-
- // Fetch the data using Fetch API
-  fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const continents = {};
-    data.forEach(country => {
-      const continent = country.region;
-      if (continent in continents) {
-        continents[continent] += 1;
-      } else {
-        continents[continent] = 1;
-      }
-    });
-    const chartData = {
-      labels: Object.keys(continents),
-      datasets: [{
-        label: 'Total Number of Countries by Continent',
-        data: Object.values(continents),
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-        borderWidth: 1
-      }]
-    };
-    const chartOptions = {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      },
-      legend: {
-        display: false
-      },
-      responsive: true,
-      maintainAspectRatio: false,
-      barThickness: 30 // Adjust the bar thickness as needed
-    };
-    const chart = new Chart(document.getElementById('chart'), {
-      type: 'bar',
-      data: chartData,
-      options: chartOptions
-    });
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
